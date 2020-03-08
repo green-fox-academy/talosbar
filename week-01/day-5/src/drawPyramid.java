2a613471a0fa543d0f41c05bd38b65c304bed098
Tálos Atanáz <talos.barbara@gmail.com>
2020-03-08 21:17:35 +0100

    I made more exercises

--- week-01/day-5/src/drawPyramid.java
+++ week-01/day-5/src/drawPyramid.java
@@ -10,14 +10,32 @@ public class drawPyramid {
         int yourNumber = scanner.nextInt();
 
         for (int i = 0; i < yourNumber; i++) {
-            for (int j = 0; j < yourNumber - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= i; k++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+            for (int j = 1; j < 2 * yourNumber; j++) {
+                if (j >= yourNumber - i && j <= yourNumber + i) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            } System.out.println();
+
+
+//        for (int i = 0; i < yourNumber; i++) {
+//            for (int j = 0; j < yourNumber - i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 0; k <= i; k++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
         }
 
     }
+
 }
+
+
+//i=0 ...*... j elemek száma a sorban, 2 * yourNumber - 1
+//i=1 ..***..
+//i=2 .*****.
+//i=3 *******
