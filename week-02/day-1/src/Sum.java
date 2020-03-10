e6873a4f8f4c2b85d2003a44f65236f81fe8051e
Tálos Atanáz <talos.barbara@gmail.com>
2020-03-10 20:46:58 +0100

    I made the code better.

--- week-02/day-1/src/Sum.java
+++ week-02/day-1/src/Sum.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 
 public class Sum {
-//    public static int sum(int yourNum) {
-//            return sum(yourNum);
-//        }
-
     public static void main(String[] args) {
         /*Scanner scanner = new Scanner(System.in);
         System.out.println("Write your number!");
         int yourNum = scanner.nextInt();*/
 
-
-        int[] newArray = {2, 4, 5, 7};
-
-        int sum = 0;
-        for (int i = 0; i < newArray.length; i++) {
-            sum = sum + newArray[i];
-        }
-
-        System.out.println(sum);
+        printSum(3);
+        printSum(5);
+        printSum(13);
     }
+
+    public static void printSum(int yourNum){
+        System.out.println(sum(yourNum));
+    }
+
+    public static int sum(int yourNum) {
+        int sum = 0;
+        for (int i = 0; i < yourNum; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
 }
 
 // Create the usual class wrapper and main method on your own.
