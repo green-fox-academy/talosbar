0a777836e96f264d94fe0fe22821b6164f8b1442
Tálos Atanáz <talos.barbara@gmail.com>
2020-03-20 16:10:38 +0100

    I tested my sum of elements.

---
+++ week-03/day-5/Test/Sum/SumTest.java
@@ -0,0 +1,37 @@
+package Sum;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class SumTest {
+
+  Sum sum = new Sum();
+
+  @Test
+  public void getSumWithEmptyList() {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList());
+    assertEquals(55, sum.getSum(numbers));
+  }
+
+  @Test
+  public void getSumWithOneElement() {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(2));
+    assertEquals(2, sum.getSum(numbers));
+  }
+
+  @Test
+  public void getSumWithMultipleElements() {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(2, 23, 30));
+    assertEquals(55, sum.getSum(numbers));
+  }
+
+  @Test
+  public void getSumWithNull() {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0));
+    assertEquals(0, sum.getSum(numbers));
+  }
+
+}
