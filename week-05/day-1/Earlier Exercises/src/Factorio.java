08802d2c48dbb57e6a729dd6fdf3c3726d409509
Tálos Atanáz <talos.barbara@gmail.com>
2020-03-31 17:05:31 +0200

    I returned the input's factorial (task of second week).

--- week-05/day-1/Earlier Exercises/src/Factorio.java
+++ week-05/day-1/Earlier Exercises/src/Factorio.java
@@ -11,7 +11,7 @@ public class Factorio {
 
   public static int factorio(int yourNumber) {
     int factorial = 1;
-    for (int i = 1; i < yourNumber; i++) {
+    for (int i = 1; i <= yourNumber; i++) {
       factorial = factorial * i;
     }
     return factorial;
