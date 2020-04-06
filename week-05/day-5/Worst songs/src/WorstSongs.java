753205ba94d58a3d2cd36b2bd3e58e394fdd798d
Tálos Atanáz <talos.barbara@gmail.com>
2020-04-06 13:17:46 +0200

    Handling exception in the exercise is more corrected.

--- week-05/day-5/Worst songs/src/WorstSongs.java
+++ week-05/day-5/Worst songs/src/WorstSongs.java
@@ -20,7 +20,7 @@ public class WorstSongs {
   private static String getFileContent(String fileName) {
     //elolvassa a fájlt
     Path filePath = Paths.get("assets/" + fileName);
-    List<String> lines;
+    List<String> lines = null;
     HashMap<String, String> songTitleWithReleaseYear = new HashMap<>();
     HashMap<String, Integer> countOccurrence = new HashMap<>();
     Map.Entry<String, Integer> maxEntry = null;
@@ -28,35 +28,34 @@ public class WorstSongs {
       lines = Files.readAllLines(filePath);
     } catch (IOException e) {
       System.out.println("The file doesn't exist");
-      throw new RuntimeException();
-      //  System.exit(null);
-      //      System.exit
+      //System.exit(0);
     }
     //csinál egy hashmapet csak a dalcímből és a megjelenési évből
     //dalcím nem kell, sok változó
-    for (String line : lines) {
-      String[] data = line.split(";");
-      songTitleWithReleaseYear.put(data[1], data[3]);
-    }
-    //végig kellene menni a hashmapen, venni a value-kat sorban, és összehasonlítani
-    for (String key : songTitleWithReleaseYear.keySet()) {
-      String releaseYear = songTitleWithReleaseYear.get(key);
-      int counter = 1;
-      if (countOccurrence.containsKey(releaseYear)) {
-        counter = countOccurrence.get(releaseYear) + 1;
+    if (lines != null) {
+      for (String line : lines) {
+        String[] data = line.split(";");
+        songTitleWithReleaseYear.put(data[1], data[3]);
       }
-      countOccurrence.put(releaseYear, counter);
-    }
-    //kiszedni a max counter értéket, és a hozzá tartozó évszámot visszaadni
-    for (Map.Entry<String, Integer> entry : countOccurrence.entrySet()) {
-      if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-        maxEntry = entry;
+      //végig kellene menni a hashmapen, venni a value-kat sorban, és összehasonlítani
+      for (String key : songTitleWithReleaseYear.keySet()) {
+        String releaseYear = songTitleWithReleaseYear.get(key);
+        int counter = 1;
+        if (countOccurrence.containsKey(releaseYear)) {
+          counter = countOccurrence.get(releaseYear) + 1;
+        }
+        countOccurrence.put(releaseYear, counter);
+      }
+      //kiszedni a max counter értéket, és a hozzá tartozó évszámot visszaadni
+      for (Map.Entry<String, Integer> entry : countOccurrence.entrySet()) {
+        if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+          maxEntry = entry;
+        }
       }
     }
     return maxEntry.getKey();
   }
 }
-
 
 
 
