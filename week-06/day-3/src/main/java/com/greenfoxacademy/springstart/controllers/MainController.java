0fe51caada7b428d223be7657aad796fff55aa7d
Tálos Atanáz <talos.barbara@gmail.com>
2020-04-19 22:12:27 +0200

    RequestMapping is corrected with given method.

--- week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/MainController.java
+++ week-06/day-3/src/main/java/com/greenfoxacademy/springstart/controllers/MainController.java
@@ -2,13 +2,14 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-  @RequestMapping(value="/hello")
   @ResponseBody
+  @RequestMapping(value="/hello", method= RequestMethod.GET)
   public String hello() {
     return "Hello World";
   }
