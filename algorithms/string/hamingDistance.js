5c41bcd76943a0ad3a4db855bc89760d4af28302
talosati <talos.barbara@gmail.com>
2020-07-11 13:46:46 +0200

    functions can get words with haming distance, can remove white spaces and can multiply matrixes.

---
+++ algorithms/string/hamingDistance.js
@@ -0,0 +1,22 @@
+let stringToCompare = 'alma';
+let listToCompare = ['alms', 'sas', 'kslw', 'alom'];
+
+function calculateHamingDistance(word, string) {
+    return word.split('').reduce(
+        (distance, char, i) => {
+            // if (char !== string.charAt(i)) {
+            //     distance++;
+            // }
+            return distance + ((char !== string.charAt(i)) ? 1 : 0);
+        }, 0);
+}
+
+function countDistance(string, list) {
+    return list
+        .filter(word => word.length === string.length)
+        .filter(word => {
+            return calculateHamingDistance(word, string) <= 3;
+        });
+}
+
+console.log(countDistance(stringToCompare, listToCompare));
